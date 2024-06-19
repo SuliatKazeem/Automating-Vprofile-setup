aafd20ea0b377cd14940d2c099408c298c3e2ca2
SuliatKazeem <suliatkaz@gmail.com>
2024-06-19 11:01:12 +0100

    Updated

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.10"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.32.0"]]],"checksum":"f526305055afc4f53f185d995a0f11f39ed59531efc22b1f8e88c86ff79c2311","vagrant_version":"2.3.7"}
